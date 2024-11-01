85b6cddf1da00d248057e089eaa76cc86d2df0f9
suiryuunin <suiryuu.nin@gmail.com>
2024-10-31 22:27:14 -0400

    added my old nasty settings code

---
+++ Engine/inputs.js
@@ -0,0 +1,270 @@
+let option = undefined;
+
+const keyDown = (e) => {
+
+    if (Math.floor(gameState) != 0 && !mobile) {
+
+        if ((e.key.length == 1 || (e.key == " " && !display.input.includes(" "))) && display.input.length < maxLetters) {
+    
+            if (display.input == "...")
+                display.input = "";
+
+            if (e.code == "Backquote")
+                display.input += "∞";
+            else if ( !(e.code == "Space" && (settings.check == 1 || settings.check == 2) ) || gameState == -1)
+                display.input += e.key;
+    
+        }
+    }
+    
+    if (Math.floor(gameState) != 0 && !mobile)
+    {
+        if (display.input != "...") {
+    
+            switch (e.code) {
+        
+                case "ControlLeft": case "ControlRight":
+                    ctrl = true;
+                    break;
+        
+                case "Backspace":
+
+                    display.input = display.input.substring(0, display.input.length - 1);
+
+                    if (ctrl) {
+                        if (display.input.includes(" ")) {
+                            
+                            display.input = display.input.split(" ")[0];
+        
+                        } else {
+        
+                            display.input = "";
+                            
+                        }
+                        
+                    }
+                    break;
+
+                case "Space":
+
+                    if ((settings.check != 0) && gameState != -1)
+                    {
+                        if (option == undefined)
+                            option = check();
+                        else
+                            check();
+
+                        display.input = "";
+                        if (mobile)
+                            input.value = "";
+                    }
+                    break;
+
+                case "Enter":
+
+                    if (settings.check != 1 || gameState == -1)
+                    {
+                        if (option == undefined)
+                            option = check();
+                        else
+                            check();
+
+                        display.input = "";
+                        if (mobile)
+                            input.value = "";
+                    }
+                    break;
+        
+            }
+    
+        }
+        
+        for (let i = 0; i < words.length; i++) {
+            if (words[i] != undefined) {
+                if (display.input == words[i].word.substring(0, display.input.length))
+                    words[i].overlay = display.input;
+                else
+                    words[i].overlay = '';
+            }
+        }
+
+    }
+
+    if (mobile)
+    {
+        switch (e.keyCode) {
+            
+            case 32:
+
+                if ((settings.check != 0) && gameState != -1)
+                {
+                    if (option == undefined)
+                        option = check();
+                    else
+                        check();
+
+                    display.input = "";
+                    if (mobile)
+                        input.value = "";
+                }
+                break;
+
+            case 13:
+
+                if (settings.check != 1 || gameState == -1)
+                {
+                    if (option == undefined)
+                        option = check();
+                    else
+                        check();
+
+                    display.input = "";
+                    if (mobile)
+                        input.value = "";
+                }
+                break;
+    
+        }
+    }
+
+}
+
+const keyUp = (e) => {
+
+    switch (e.code) {
+
+        case "ControlLeft": case "ControlRight":
+            ctrl = false;
+            break;
+
+        case "Escape":
+            switch(Math.floor(gameState)) {
+
+                case 1:
+                    gameState = 0 + lastPage;
+                    document.getElementById('borderS').style.display = 'block';
+
+                    break;
+
+                case 0:
+                    gameState = 1;
+                    document.getElementById('borderS').style.display = 'none';
+
+                    break;
+
+            }
+
+            if (
+                (
+                settings.old[0] != settings.text     ||
+                settings.old[1] != settings.genMode  ||
+                settings.old[2] != settings.gameMode ||
+                settings.old[3] != settings.words    ||
+                settings.old[4] != settings.maxChar) &&
+                gameState != -1                        )
+                setup();
+
+            break;
+
+    }
+
+}
+
+const mouseDown = (e) => {
+
+    if (Math.floor(gameState) == 0)
+        mouseInput = new MouseInput(e);
+
+}
+
+const mouseUp = () => {
+
+    
+    if (mouseInput != undefined && mouseInput.element != undefined && mouseInput.element.alpha == 1 && mouseInput.element.type == "options") {
+
+        mouseInput.element.index *= 1;
+        mouseInput.element.index = (mouseInput.element.index + 1) % mouseInput.element.options.length;
+        
+        if (mouseInput.element.changed === false)
+            mouseInput.element.changed = true;
+
+    }
+
+    if (mouseInput != undefined && mouseInput.element != undefined && mouseInput.element.type == "button") {
+
+        mouseInput.element.action();
+
+    }
+
+    mouseInput = undefined;
+
+}
+
+class MouseInput {
+
+    constructor(e) {
+
+        let page = pages[Math.floor(gameState * 10 - 1)];
+
+        this.updateValue = function() {
+
+            if (this.element.type == "slider")
+                this.element.value = Math.ceil((this.element.x - (this.element.fixedPos[0] - Math.floor(this.element.width / 2))) / (this.element.barWidth / this.element.minMax[1])) + this.element.valueOffset;
+
+        };
+        
+        this.getElement = function() {
+
+            for (let i = 0; i < page.length; i++) {
+
+                if ( page[i].alpha == 1 && page[i].type == "slider" && ((e.clientX - edge[0] > Math.floor((page[i].fixedPos[0] / 384 * 0.8 + 0.1) * display.display.canvas.width) && e.clientX - edge[0] < Math.floor(((page[i].fixedPos[0] + page[i].barWidth) / 384 * 0.8 + 0.1) * display.display.canvas.width)) && (e.clientY - edge[1] > Math.floor((page[i].fixedPos[1] / 288 * 0.8 + 0.1) * display.display.canvas.height) && e.clientY - edge[1] < Math.floor(((page[i].fixedPos[1] + page[i].height) / 288 * 0.8 + 0.1) * display.display.canvas.height))) ) {
+
+                    page[i].x = Math.floor( Math.round( ( ( (((e.clientX - edge[0]) / display.display.canvas.width - 0.1) / 0.8 * 384) ) - page[i].fixedPos[0]) / (page[i].barWidth / page[i].minMax[1])) * (page[i].barWidth / page[i].minMax[1]) + page[i].fixedPos[0] - page[i].width / 2);
+                    return (page[i]);
+
+                }
+
+
+                if ((e.clientX - edge[0] > Math.floor((page[i].x / 384 * 0.8 + 0.1) * display.display.canvas.width) && e.clientX - edge[0] < Math.floor(((page[i].x + page[i].width) / 384 * 0.8 + 0.1) * display.display.canvas.width)) && (e.clientY - edge[1] > Math.floor((page[i].y / 288 * 0.8 + 0.1) * display.display.canvas.height) && e.clientY - edge[1] < Math.floor(((page[i].y + page[i].height) / 288 * 0.8 + 0.1) * display.display.canvas.height)))
+                    return(page[i]);
+
+                
+            }
+            
+        };
+        
+        this.element = this.getElement();
+
+        if (this.element != undefined)
+            this.updateValue();
+
+    }
+    
+    move(e) {
+    
+        if (mouseInput != undefined && mouseInput.element != undefined && mouseInput.element.alpha == 1 && mouseInput.element.type == "slider") {
+            
+            if (e.clientX - edge[0] > Math.floor((mouseInput.element.fixedPos[0] / 384 * 0.8 + 0.1) * display.display.canvas.width - (mouseInput.element.width / 2 - 4)) && e.clientX - edge[0] < Math.floor(((mouseInput.element.fixedPos[0] + mouseInput.element.barWidth) / 384 * 0.8 + 0.1) * display.display.canvas.width) )
+                mouseInput.element.x = Math.floor( Math.round( ( ( (((e.clientX - edge[0]) / display.display.canvas.width - 0.1) / 0.8 * 384) ) - mouseInput.element.fixedPos[0]) / (mouseInput.element.barWidth / mouseInput.element.minMax[1])) * (mouseInput.element.barWidth / mouseInput.element.minMax[1]) + mouseInput.element.fixedPos[0] - mouseInput.element.width / 2);
+
+
+            if (e.clientX - edge[0] < Math.floor((mouseInput.element.fixedPos[0] / 384 * 0.8 + 0.1) * display.display.canvas.width - (mouseInput.element.width / 2 - 4)) )
+                mouseInput.element.x = mouseInput.element.fixedPos[0] - Math.floor(mouseInput.element.width / 2);
+
+            if (e.clientX - edge[0] > Math.floor(((mouseInput.element.fixedPos[0] + mouseInput.element.barWidth) / 384 * 0.8 + 0.1) * display.display.canvas.width) )
+                mouseInput.element.x = mouseInput.element.fixedPos[0] + mouseInput.element.barWidth - Math.floor(mouseInput.element.width / 2);
+
+            mouseInput.updateValue();
+            if (mouseInput.element.changed === false)
+                mouseInput.element.changed = true;
+        }
+    
+    }
+
+}
+let mouseInput = undefined;
+
+window.addEventListener("keydown", keyDown);
+window.addEventListener("keyup", keyUp);
+window.addEventListener("mousedown", mouseDown);
+window.addEventListener("mouseup", mouseUp);
---
+++ Engine/settings.js
@@ -0,0 +1,190 @@
+class Settings {
+
+    constructor() {
+
+        // Default values
+        // Setup
+        this.text = "Genesis";
+        this.genMode = 0;
+        this.gameMode = 0;
+        this.oGameMode = 0;
+        this.words = 30;
+        this.maxChar = 30;
+        
+        // Gameplay
+        this.maxWords = 3;
+        this.interval = 50;
+        this.speed = 2;
+        this.bF = 0;
+        this.ceil = 0;
+        this.health = 3;
+        this.caps = false;
+        this.auto = 1;
+        this.old = [this.text, this.genMode, this.gameMode, this.words, this.maxChar];
+        this.check = false;
+
+    }
+
+    updateOld()
+    {
+        this.old = [this.text, this.genMode, this.gameMode, this.words, this.maxChar];
+    }
+
+    modify(/* Gameplay -> */ maxWords, interval, speed, caps, auto, space, bF, ceil,
+        /* Setup -> */ text = this.text, genMode = this.genMode, gameMode = this.gameMode, words = this.words, maxChar = this.maxChar)
+    {
+
+        this.maxWords = maxWords;
+        this.interval = interval;
+        this.speed = speed;
+        this.caps = caps;
+        this.auto = auto;
+        this.check = space;
+        this.bF = bF;
+        this.ceil = ceil;
+
+        this.text = text;
+        this.genMode = genMode;
+        this.gameMode = gameMode;
+        this.words = words;
+        this.maxChar = maxChar;
+
+    }
+    
+}
+
+class Slider {
+
+    constructor(setting, x, y, width, height, display, value = 0, minMax, valueOffset = 0, alpha = 1) {
+
+        this.type = "slider";
+
+        this.setting = setting;
+
+        this.x = x + 60;
+        this.y = y;
+        this.barX = x + Math.floor(width / 2);
+        this.barY = y + Math.floor(height / 2);
+        this.fixedPos = [this.x + width / 2, this.y];
+        this.width = width;
+        this.height = height;
+        this.display = display;
+        this.value = value;
+        this.fake;
+        this.minMax = minMax;
+        this.valueOffset = valueOffset;
+
+        this.alpha = alpha
+
+        this.barWidth = this.width + Math.floor(this.display.settings.canvas.width / 2) - 48;
+
+    }
+
+    updatePos() {
+
+        this.display.createWord(this.display.settings, this.setting, 48, this.y - 5, 0, false, 1, 16, this.alpha);
+
+        this.display.createRect(this.display.settings, this.x, this.y, this.width, this.height, this.display.color, 1, this.alpha);
+        this.display.createRect(this.display.settings, this.x, this.y, this.width, this.height, this.display.color, 1, this.alpha);
+        this.display.createRect(this.display.settings, this.fixedPos[0], this.fixedPos[1] + 3, this.barWidth, this.height - 6, this.display.color, 1, this.alpha);
+        
+        this.display.createWord(this.display.settings, (this.fake == undefined ? this.value : this.fake), 188, this.y - 5, -0.5, false, 1, 16, this.alpha);
+        this.display.createFill(this.display.settings, this.x, this.y, this.width, this.height, 0.5 - (1-this.alpha) / 2)
+
+    }
+
+}
+
+class Options {
+
+    constructor(setting, x, y, display, options, index = 0, offsetX = 0, alpha = 1) {
+
+        this.type = "options";
+
+        this.setting = setting;
+
+        this.x = x + 168;
+        this.y = y - 5;
+        this.fixedPos = [this.x, this.y];
+        this.offsetX = offsetX;
+
+        this.display = display;
+        this.options = options;
+        this.index = index;
+
+        this.display.settings.font = `16px MisterPixel`;
+        this.width = this.display.settings.measureText(this.options[this.index])["width"];
+        this.height = 16;
+
+        this.alpha = alpha
+
+    }
+
+    updatePos() {
+
+        this.display.settings.font = `16px MisterPixel`;
+        this.width = this.display.settings.measureText(this.options[this.index])["width"];
+
+        this.x = this.fixedPos[0] + this.offsetX * this.width + 2;
+
+        this.display.createWord(this.display.settings, this.setting, 48, this.y, 0, false, 1, 16, this.alpha);
+        this.display.createWord(this.display.settings, this.options[this.index], this.fixedPos[0], this.y, this.offsetX, true, 1, 16, this.alpha);
+
+    }
+
+}
+
+class Button {
+
+    constructor(x, y, display, text, offsetX = 0, enabled, pageChange) {
+
+        this.type = "button";
+
+        this.text = text;
+
+        this.x = x;
+        this.y = y - 6;
+        this.fixedPos = [this.x, this.y];
+        this.offsetX = offsetX;
+        this.enabled = enabled;
+
+        this.display = display;
+
+        this.display.settings.font = `16px MisterPixel`;
+        this.width = this.display.settings.measureText(this.text)["width"];
+        this.height = 16;
+
+        this.action = () => {
+
+            if (enabled) {
+
+                if (pageChange * 0 == 0)
+                {
+                    gameState += pageChange;
+                    lastPage += pageChange;
+                }
+                else
+                {
+                    document.getElementById('borderS').style.display = 'none';
+                    
+                    INIT();
+                }
+
+            }
+        
+        }
+
+    }
+
+    updatePos() {
+
+        this.display.settings.font = `16px MisterPixel`;
+        this.width = this.display.settings.measureText(this.text)["width"];
+
+        this.x = this.fixedPos[0] + this.offsetX * this.width + 2;
+
+        this.display.createWord(this.display.settings, this.text, this.fixedPos[0], this.y, this.offsetX, false, 1, 16, (this.enabled) ? 1 : 0.5);
+
+    }
+
+}
